e914f02ea3296f094bf9d93a82f21fef73ab2986
JYeon <doryJYeon@gmail.com>
2024-04-01 00:38:57 +0900

    [fix] v0

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ const App = () => {
         />
       </div>
       <div className="game-info">
-        <div className={`status ${winner && "winner"}`}>{status}</div>
+        <div className="status">{status}</div>
         <ol>{moves}</ol>
       </div>
     </div>
